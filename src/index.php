3950a160b42de4a3b57353344d9b71843fba6f15
Robert Ingrum <ringrum@purplewall.com>
2016-04-15 15:46:22 -0700

    Adding the basics.

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>For Us, The Living</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Bitter:400,400italic,700' rel='stylesheet' type='text/css' />
+        <link href="static/style/base.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <section id="header">
+            <h1><span class="title-part-1">For Us,</span> <span class="title-part-2">The Living</span></h1>
+            <h2>This tagline changes every time...</h2>
+            <img src="static/images/paper.svg" alt="paper" />
+        </section>
+        <section id="about">
+
+        </section>
+    </body>
+</html>
