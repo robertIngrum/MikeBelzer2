72bacbbc0339f10c8f820d7e0efa99433c38415c
Robert Ingrum <ringrum@purplewall.com>
2016-04-18 16:11:27 -0700

    Added basic animations for resume.

--- src/index.php
+++ src/index.php
@@ -6,12 +6,21 @@
 
         <link href='https://fonts.googleapis.com/css?family=Bitter:400,400italic,700' rel='stylesheet' type='text/css' />
         <link href="static/style/base.css" rel="stylesheet" type="text/css" />
+
+        <script src="static/libs/jquery/jquery-2.1.4.min.js" type="application/javascript"></script>
+        <script src="static/scripts/drawings.js" type="application/javascript"></script>
     </head>
     <body>
         <section id="header">
             <h1><span class="title-part-1">For Us,</span> <span class="title-part-2">The Living</span></h1>
             <h2>This tagline changes every time...</h2>
-            <img src="static/images/paper.svg" alt="paper" />
+
+            <div class="resume">
+                <img class="text" src="static/images/resume_text.svg" alt="Click here to view my resume!" />
+                <img class="arrow" src="static/images/arrow.svg" alt="arrow" />
+                <img class="paper" src="static/images/paper.svg" alt="paper" />
+                <img class="circle" src="static/images/circle.svg" alt="circle" />
+            </div>
         </section>
         <section id="about">
 
