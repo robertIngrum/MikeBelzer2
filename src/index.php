43a95deccafdacdcb41aaee8d656354064e7b8a6
Robert Ingrum <ringrum@purplewall.com>
2016-04-20 14:49:24 -0700

    Adding images to Project.

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
     <body>
         <section id="header">
             <h1><span class="title-part-1">For Us,</span> <span class="title-part-2">The Living</span></h1>
-            <h2><?php echo $chosen_tagline ?></h2>
+            <h3><?php echo $chosen_tagline ?></h3>
 
             <div class="resume">
                 <img class="text" src="static/images/resume_text.svg" alt="Click here to view my resume!" />
@@ -29,7 +29,60 @@
             </div>
         </section>
         <section id="about">
-
+            <p>
+                Hello!
+            </p>
+            <p>
+                My name's Robert Ingrum and I'm a
+                <?php echo substr(date('Ymd') - date('Ymd', strtotime('1994-03-12')), 0, -4); ?>
+                year old developer currently living in Seattle, Washington.  This is my personal website and will serve
+                as a hub of sorts for all sorts of fun and interesting content that I'm involved with.  For example,
+                you can find a link to the startup I'm currently working with below under
+                <a href="#projects">Projects</a>.
+            </p>
+            <p>
+                I also do freelance and consulting, so if you're looking for an experienced programmer / designer /
+                system administrator, look no further!  Shoot me an email at
+                <a href="mailto:robert@forustheliving.com">robert@forustheliving.com</a>.  You can find my professional
+                resume at the top right of the window if you'd like something a more official background.
+            </p>
+        </section>
+        <section id="projects">
+            <h2>Projects</h2>
+            <div class="project-list">
+                <a href="https://www.purplewall.com"
+                   style="background-image: url('static/images/projects/purplewall.png');"
+                   target="_blank">
+                    <span>PurpleWall</span>
+                </a>
+                <a href="https://blog.purplewall.com"
+                   style="background-image: url('static/images/projects/purplewall_blog.png');"
+                   target="_blank">
+                    <span>PurpleWall Blog</span>
+                </a>
+                <a href="http://www.holmesautopilot.com/"
+                   style="background-image: url('static/images/projects/holmes_autopilot.png');"
+                   target="_blank">
+                    <span>Holmes AutoPilot</span>
+                </a>
+                <a href="https://biz.colostate.edu"
+                   style="background-image: url('static/images/projects/csu.png');"
+                   target="_blank">
+                    <span>Colorado State University</span>
+                </a>
+                <a href="http://www.mikebelzer.com"
+                   style="background-image: url('static/images/projects/mikebelzer.png');"
+                   target="_blank">
+                    <span>MikeBelzer.com</span>
+                </a>
+            </div>
+        </section>
+        <section id="footer">
+            <p>
+                That's it!  If you'd like more you can check out my
+                <a href="blog.forustheliving.com" target="_blank">blog</a> or send me an email at
+                <a href="mailto:robert@forustheliving.com">robert@forustheliving.com</a>.
+            </p>
         </section>
     </body>
 </html>
--- src/static/scripts/drawings.js
+++ src/static/scripts/drawings.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-
+    $('.resume').click(function() { window.open('/static/images/robert_ingrum_resume.pdf') });
 });
