6be4b0ca2079803f75792499606f091638ecadeb
Robert Ingrum <rkingrum@gmail.com>
2017-01-17 19:37:00 -0700

    Progress

--- blog/wp-content/themes/futl/header.php
+++ blog/wp-content/themes/futl/header.php
@@ -17,23 +17,12 @@
     </head>
     <body>
         <section id="header">
-            <?php
-                # Styling the title of the blog
-                $blog_name = get_bloginfo('name');
-                $blog_name_split = explode('.', $blog_name);
-                $blog_name =
-                    "<span class='title-part-1'>{$blog_name_split[0]}</span><span class='title-part-2'>.{$blog_name_split[1]}</span>";
-            ?>
-
             <h1>
-                <a href="<?php echo get_option('home'); ?>">
-                    <?php echo $blog_name; ?>
+                <a href="<?php echo get_option('home'); ?>" id="site-logo">
+                    <?php echo get_bloginfo('name'); ?>
                 </a>
             </h1>
-
-            <section id="navigation">
-                <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
-            </section>
         </section>
-    </body>
-</html>
+        <section id="navigation">
+            <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
+        </section>
