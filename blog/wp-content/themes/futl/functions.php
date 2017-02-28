702b42f9b353720f932a1f4436411ab888bec5e3
Robert Ingrum <rkingrum@gmail.com>
2017-02-27 23:54:44 -0700

    Updaing I don't even know

--- blog/wp-content/themes/futl/functions.php
+++ blog/wp-content/themes/futl/functions.php
@@ -1,5 +1,9 @@
 <?php
     add_filter('stylesheet_uri', 'wpi_stylesheet_uri', 10, 2);
+    add_filter('pre_kses', 'add_scriptfilter');
+
+    remove_filter( 'the_content', 'wpautop' );
+    remove_filter( 'the_excerpt', 'wpautop' );
 
     add_theme_support( 'post-thumbnails' );
 
@@ -20,4 +24,10 @@
             'before_title' => '<h2>',
             'after_title' => '</h2>'
         ));
+    }
+
+    function add_scriptfilter( $string ) {
+        global $allowedtags;
+        $allowedtags['script'] = array( 'src' => array() );
+        return $string;
     }
